1624a0539b24068a99ad7d523df2da2f4670e104
Tibor Szabo <tiborszabo@Tibors-MacBook-Pro.local>
2019-11-16 17:36:19 +0100

    Fixing unit tests

--- test/test.js
+++ test/test.js
@@ -1,12 +1,38 @@
 
 var expect = require("chai").expect;
 var oceanex = require('../oceanex-lib.js');
-describe('getMarkets', function() {
-    it("Using a Promise that resolves successfully!", function(done) {
-        var testPromise = oceanex.getMarkets()
-        testPromise.then(function(result) {
-            expect(result).to.length > 0
-            done();
+describe('Public endpoints', function() {
+    it("Check getMarkets()", function(done) {
+        var checker = oceanex.getMarkets()
+        checker.then(function(result){
+            try {
+                expect(result.code).to.equal(0);
+                done();
+            } catch(err) {
+                done(err);
+            }
+        }, done);
+    });
+    it("Check getTicker() for VET/BTC market", function(done) {
+        var checker = oceanex.getTicker('vetbtc')
+        checker.then(function(result){
+            try {
+                expect(result.code).to.equal(0);
+                done();
+            } catch(err) {
+                done(err);
+            }
+        }, done);
+    });
+    it("Check getAllTickers()", function(done) {
+        var checker = oceanex.getAllTickers()
+        checker.then(function(result){
+            try {
+                expect(result.code).to.equal(0);
+                done();
+            } catch(err) {
+                done(err);
+            }
         }, done);
     });
 });
