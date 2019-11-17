4aa373226f3d1af1ee69674172dd9c62a63180aa
Tibor Szabo <tiborszabo@Tibors-MacBook-Pro.local>
2019-11-17 22:13:42 +0100

    more private queries

--- oceanex-lib.js
+++ oceanex-lib.js
@@ -77,5 +77,76 @@ exports.OceanEx = class  {
         let holdings = await this.privateGetQuery('https://api.oceanex.pro/v1/members/me?' , getHoldings);
         return holdings
     }
+
+    async createTrade(market, side, volume, price, type = 'limit')
+    {
+        console.log('> Attempting to create trade market: ' + market + ' side: ' + side + ' volume: ' + volume + ' price: ' + price)
+
+        let createTrade={
+            "uid": this.UID,
+            "data": {
+            "market" : market,
+            "side" : side,
+            "price" : price,
+            "volume": volume,
+            "ord_type" : type
+            }
+        }
+
+        let tradeResult = await this.privatePostQuery('https://api.oceanex.pro/v1/orders?' , createTrade);
+
+        if (tradeResult.code == 0) {
+            return tradeResult;
+        }
+
+        // FIXME:
+        console.log("Something went wrong !!!!!! ")
+        console.log(tradeResult)
+        return tradeResult;
+    }
+
+    async cancelTrade(id)
+    {
+        console.log('Attempting to cancel trade id: ' + id )
+
+        let cancelTrade={
+            "uid": this.UID,
+            "data": {
+            "id" : id
+            }
+        }
+
+        let result = await this.privatePostQuery('https://api.oceanex.pro/v1/order/delete?' , cancelTrade);
+
+        if (result.data.state == 'done') {
+            console.log("Trade cancelled!")
+            console.log(result.data)
+        }
+        else {
+            console.log("Something went wrong !!!!!!")
+            console.log(result)
+        }
+        return result.data;
+    }
+
+    async getOrderStatus(id)
+    {
+        //console.log('Attempting to get status of trade id: ' + id )
+
+        let data = {
+            "uid": UID,
+            "data": {
+            "ids" : [id]
+            }
+        }
+
+        let result = await this.privateGetQuery(' https://api.oceanex.pro/v1/orders?' , data);
+
+        if (result.code != 0) {
+            console.log(result)
+        }
+        
+        return result.data;
+    }
 }
 
