8b589f57adc3dc53a70c84c5ce041b826fb6d91d
Tibor Szabo <tiborszabo@Tibors-MBP.localdomain>
2020-05-05 23:38:52 +0200

    test

--- test/test.js
+++ test/test.js
@@ -18,7 +18,7 @@ describe('Public endpoints', function() {
         var checker = oceanex.getTicker('vetbtc')
         checker.then(function(result){
             try {
-                console.log('res: '+ result)
+                console.log('res: '+ JSON.stringify(result))
                 expect(result.code).to.equal(0);
                 done();
             } catch(err) {
